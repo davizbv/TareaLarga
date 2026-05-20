f9bdbed5dff6f1ea41a9f27ab09a2d4f6fb05f28
davizbv <davizzbv@gmail.com>
2026-05-20 18:55:46 +0200

    Add files via upload

---
+++ php.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Tarea Larga
+ * Description: Este es mi plugin para la tarea.
+ * Version: 1.0
+ * Author: Adrian, Dennis y David
+ */
+
+// Aquí debajo va el código que hace funcionar al plugin
+echo "<h1>¡Hola! Este es mi archivo PHP de TareaLarga si</h1>";
+?>
